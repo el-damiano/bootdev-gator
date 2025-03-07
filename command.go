d226cbf335ebb7daf19b5db893b6cb8d6bd38c07
el-damiano <el9damiano@gmail.com>
2025-03-07 21:18:46 +0100

    feat: add login command

---
+++ command.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type command struct {
+	Name string
+	Args []string
+}
+
+func handlerLogin(state *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("command %s expects [name] argument", cmd.Name)
+	}
+	username := cmd.Args[0]
+	err := state.Config.SetUser(username)
+	if err != nil {
+		return err
+	}
+	log.Printf("%s set as current user\n", username)
+	return nil
+}
+
+type commandRegistry struct {
+	reg map[string]func(*state, command) error
+}
+
+func (cmdReg *commandRegistry) register(name string, function func(*state, command) error) {
+	cmdReg.reg[name] = function
+}
+
+func (cmdReg *commandRegistry) run(s *state, cmd command) error {
+	command, ok := cmdReg.reg[cmd.Name]
+	if !ok {
+		return fmt.Errorf("command %s not found\n", cmd.Name)
+	}
+	err := command(s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,20 +3,47 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/el-damiano/bootdev-gator/internal/config"
 )
 
+type state struct {
+	Config *config.Config
+}
+
 func main() {
+
 	configFile, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading file %v\n", err)
 	}
 	fmt.Printf("read config %+v\n", configFile)
 
-	err = configFile.SetUser("damian")
+	mainState := &state{
+		Config: &configFile,
+	}
+	_ = mainState
+
+	commands := commandRegistry{
+		reg: map[string]func(*state, command) error{},
+	}
+
+	sysArgs := os.Args
+	if len(sysArgs) < 2 {
+		log.Fatalf("not enough arguments were provided, wanted 2 but got %d\n", len(sysArgs))
+		os.Exit(1)
+	}
+
+	commandLogin := &command{
+		Name: sysArgs[1],
+		Args: sysArgs[2:],
+	}
+	commands.register(commandLogin.Name, handlerLogin)
+	err = commands.run(mainState, *commandLogin)
 	if err != nil {
-		log.Fatalf("error setting user %v\n", err)
+		log.Fatalf("%v", err)
+		os.Exit(1)
 	}
 
 	configFile, err = config.Read()
