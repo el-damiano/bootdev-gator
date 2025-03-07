f88e4688ffcc6bf3dfb57a66c503acc4486e0024
el-damiano <el9damiano@gmail.com>
2025-03-07 15:19:55 +0100

    chore: add logs for configuration settings

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/el-damiano/bootdev-gator/internal/config"
 )
@@ -9,17 +10,18 @@ import (
 func main() {
 	configFile, err := config.Read()
 	if err != nil {
-		fmt.Printf("error reading file %s\n", err)
+		log.Fatalf("error reading file %v\n", err)
 	}
+	fmt.Printf("read config %+v\n", configFile)
 
 	err = configFile.SetUser("damian")
 	if err != nil {
-		fmt.Printf("error setting user %s\n", err)
+		log.Fatalf("error setting user %v\n", err)
 	}
 
 	configFile, err = config.Read()
 	if err != nil {
-		fmt.Printf("error reading file %s\n", err)
+		log.Fatalf("error reading file %v\n", err)
 	}
 
 	fmt.Printf("new config set %+v\n", configFile)
