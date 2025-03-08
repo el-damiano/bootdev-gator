e11deec8b33719de7354250c051b6c5c09797309
el-damiano <el9damiano@gmail.com>
2025-03-08 18:51:44 +0100

    refactor: add timeout and change return values when fetching feeds

--- command.go
+++ command.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"context"
-	"encoding/xml"
 	"fmt"
-	"html"
-	"io"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/el-damiano/bootdev-gator/internal/database"
@@ -122,55 +118,4 @@ func (cmdReg *commandRegistry) run(s *state, cmd command) error {
 		return fmt.Errorf("command %s not found\n", cmd.Name)
 	}
 	return command(s, cmd)
-}
-
-type RSSFeed struct {
-	Channel struct {
-		Title       string    `xml:"title"`
-		Link        string    `xml:"link"`
-		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
-	} `xml:"channel"`
-}
-
-type RSSItem struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
-}
-
-func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
-	if err != nil {
-		return &RSSFeed{}, err
-	}
-	req.Header.Set("User-Agent", "gator")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return &RSSFeed{}, err
-	}
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return &RSSFeed{}, err
-	}
-
-	var feed = RSSFeed{}
-	err = xml.Unmarshal(data, &feed)
-	if err != nil {
-		return &RSSFeed{}, err
-	}
-
-	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
-	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
-
-	for _, item := range feed.Channel.Item {
-		item.Title = html.UnescapeString(item.Title)
-		item.Description = html.UnescapeString(item.Description)
-	}
-
-	return &feed, nil
 }
